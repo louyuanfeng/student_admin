a5b3451bb43428349c64176c97ac11c56b6a7d3c
qf-wang <sz-wangjing@1000phone.com>
2018-07-02 23:02:15 +0800

    模块整理

--- php/del.php
+++ php/del.php
@@ -1,23 +1,14 @@
 <?php
   header('Content-Type: text/html; charset=UTF-8');
-  $id = $_GET['id'];
-  $conn= new mysqli('localhost','root','','db_student_admin','3306');
-  //2.定义sql语句
+  include "public/public_db.php";
+  $id = $_POST['id'];
+  $coon = new db();
   $sql = "delete from student_score where id = $id";
-  $conn ->query("SET CHARACTER SET 'utf8'");//读库
-  //3.发送SQL语句
-  // 删除成功返回true， 不然为false
-  $result = $conn -> query($sql);
+  $result = $coon -> Query($sql, null);
   if($result) {
-    echo "<script>
-            alert('删除成功');
-            location.href = 'manager.php';
-          </script>";
-  } else {
-    echo "<script>
-          alert('删除失败');
-          location.href = 'manager.php';
-        </script>";
+    $arr = array("msg" => "", "code" => "200");
+  }  else {
+    $arr = array("msg" => "删除失败", "code" => "1002");
   }
-  $conn->close()
+  echo json_encode($arr);
  ?>
